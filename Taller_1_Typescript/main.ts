81795f0bd15f79234d44d0a8dc8225b60e979238
Nathaliaq16 <85660281+Nathaliaq16@users.noreply.github.com>
2022-04-21 23:49:21 -0500

    3

--- Taller_1_Typescript/main.ts
+++ Taller_1_Typescript/main.ts
@@ -3,18 +3,21 @@ import { series } from "./data.js";
 
 const seriesTbody: HTMLElement = document.getElementById("series_id")!; 
 const avgSerieVar: HTMLElement = document.getElementById("avgSeasons_id")!;
+const imagen1: HTMLElement = document.getElementById("primera_imagen")!;
 
 renderSeriesInTable(series);
 avgSerieVar.innerHTML = `${getAvgSeasons(series)}`
+linkCards(series);
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
+                           <td><a href = "">${c.name}</a></td>
                            <td>${c.channel}</td>
                            <td>${c.season}</td>`;
     seriesTbody.appendChild(trElement);
+    
   });
 }
 
@@ -24,4 +27,9 @@ function getAvgSeasons(series: Serie[]): number {
     series.forEach((serie) => sumSeason = sumSeason + serie.season);
     avgSeason = sumSeason / series.length;
     return series.length;
+}
+
+function linkCards(series: Serie[]): void {
+  imagen1.setAttribute("src", "N.jpeg");
+
 }
--- Taller_1_Typescript/scripts/main.js
+++ Taller_1_Typescript/scripts/main.js
@@ -1,12 +1,14 @@
 import { series } from "./data.js";
 var seriesTbody = document.getElementById("series_id");
 var avgSerieVar = document.getElementById("avgSeasons_id");
+var imagen1 = document.getElementById("primera_imagen");
 renderSeriesInTable(series);
 avgSerieVar.innerHTML = "".concat(getAvgSeasons(series));
+linkCards(series);
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                           <td>").concat(c.name, "</td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.season, "</td>");
+        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                           <td><a href = \"\">").concat(c.name, "</a></td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.season, "</td>");
         seriesTbody.appendChild(trElement);
     });
 }
@@ -16,4 +18,7 @@ function getAvgSeasons(series) {
     series.forEach(function (serie) { return sumSeason = sumSeason + serie.season; });
     avgSeason = sumSeason / series.length;
     return series.length;
+}
+function linkCards(series) {
+    imagen1.setAttribute("src", "N.jpeg");
 }
